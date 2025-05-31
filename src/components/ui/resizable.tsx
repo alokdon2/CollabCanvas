459fba37d5aaf6b1803e0f57cce2f0947dcf1938
Wild Use Stone <stonewilduse@gmail.com>
2025-05-31 11:06:36 +0000

    the user should be able to use it to drag and resize panels. Make it work

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -1,116 +1,58 @@
 
 "use client"
 
+import { GripVerticalIcon } from "lucide-react"
 import * as React from "react"
-// import * as ResizablePrimitive from "@radix-ui/react-resizable-panels" // Commented out: package likely not installed
+import {
+  PanelGroup as PanelGroupPrimitive,
+  Panel as PanelPrimitive,
+  PanelResizeHandle as PanelResizeHandlePrimitive,
+  type PanelGroupProps as PanelGroupPrimitiveProps,
+  type PanelProps as PanelPrimitiveProps,
+  type PanelResizeHandleProps as PanelResizeHandlePrimitiveProps,
+} from "react-resizable-panels"
+
 import { cn } from "@/lib/utils"
 
-// Placeholder components to prevent crashing if @radix-ui/react-resizable-panels is not installed.
-// Actual resizing functionality will be lost.
-
 const ResizablePanelGroup = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement> & { direction?: "horizontal" | "vertical" }
->(({ className, children, direction = "horizontal", ...props }, ref) => (
-  <div
+  React.ElementRef<typeof PanelGroupPrimitive>,
+  React.ComponentPropsWithoutRef<typeof PanelGroupPrimitive>
+>(({ className, ...props }, ref) => (
+  <PanelGroupPrimitive
     ref={ref}
     className={cn(
       "flex h-full w-full data-[panel-group-direction=vertical]:flex-col",
-      direction === "vertical" ? "flex-col" : "flex-row",
       className
     )}
     {...props}
-    data-panel-group-direction={direction} // Mock attribute for potential downstream CSS
-  >
-    {children}
-  </div>
+  />
 ))
 ResizablePanelGroup.displayName = "ResizablePanelGroup"
 
-const ResizablePanel = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement> & { 
-    defaultSize?: number;
-    minSize?: number; 
-    maxSize?: number; 
-    id?: string; 
-    order?: string; 
-    collapsible?: boolean; 
-    onCollapse?: () => void; 
-    onExpand?: () => void; 
-    collapsedSize?: number;
-  }
->(({ 
-  className, 
-  children, 
-  style, 
-  defaultSize, 
-  minSize, // Destructure to prevent passing to div
-  maxSize, // Destructure
-  id,      // Destructure
-  order,   // Destructure
-  collapsible, // Destructure
-  onCollapse,  // Destructure
-  onExpand,    // Destructure
-  collapsedSize, // Destructure
-  ...props 
-}, ref) => {
-  // Attempt to respect defaultSize for basic layout, but true resizing won't work.
-  const flexStyle: React.CSSProperties = { ...style };
-  if (typeof defaultSize === 'number') {
-    flexStyle.flexGrow = defaultSize / 100;
-    flexStyle.flexShrink = 0;
-    flexStyle.flexBasis = `${defaultSize}%`;
-  } else {
-    flexStyle.flex = '1 1 0%';
-  }
+const ResizablePanel = PanelPrimitive
 
-  return (
-    <div ref={ref} className={cn("", className)} style={flexStyle} {...props}>
-      {children}
-    </div>
-  )
-})
 ResizablePanel.displayName = "ResizablePanel"
 
 const ResizableHandle = React.forwardRef<
-  HTMLButtonElement,
-  React.ButtonHTMLAttributes<HTMLButtonElement> & { withHandle?: boolean, "data-panel-group-direction"?: "horizontal" | "vertical" }
+  React.ElementRef<typeof PanelResizeHandlePrimitive>,
+  React.ComponentPropsWithoutRef<typeof PanelResizeHandlePrimitive> & {
+    withHandle?: boolean
+  }
 >(({ className, withHandle, ...props }, ref) => (
-  <button // Changed from div to button to allow disabled, but removing disabled for visibility
+  <PanelResizeHandlePrimitive
     ref={ref}
-    // disabled // Removed disabled to make it appear interactive, though it won't be functional
     className={cn(
       "relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 [&[data-panel-group-direction=vertical]>div]:rotate-90",
-      // "hidden", // Removed "hidden" class
       className
     )}
     {...props}
   >
     {withHandle && (
       <div className="z-10 flex h-4 w-3 items-center justify-center rounded-sm border bg-border">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          className="lucide lucide-grip-vertical h-2.5 w-2.5"
-        >
-          <circle cx="9" cy="12" r="1" />
-          <circle cx="9" cy="5" r="1" />
-          <circle cx="9" cy="19" r="1" />
-          <circle cx="15" cy="12" r="1" />
-          <circle cx="15" cy="5" r="1" />
-          <circle cx="15" cy="19" r="1" />
-        </svg>
+        <GripVerticalIcon className="h-2.5 w-2.5" />
       </div>
     )}
-  </button>
+  </PanelResizeHandlePrimitive>
 ))
 ResizableHandle.displayName = "ResizableHandle"
 
